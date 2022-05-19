0cef697a7ae07e3825f45da6ecb0178500cce174
ItsNotAlexy <101402577+ItsNotAlexy@users.noreply.github.com>
2022-05-19 13:50:17 +0700

    Delete root directory

--- root/scripts/discord.js
+++
@@ -1,8 +0,0 @@
-function discord() {
-    var copyText = "Alexyy#4003"
-    navigator.clipboard.writeText(copyText);
-
-    var x = document.getElementById("toolip_copied");
-    x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
